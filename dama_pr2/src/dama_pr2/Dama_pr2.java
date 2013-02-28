9cc949122f7c651b6fa74235262864dae7277469
Misa <Misa@Misa-PC>
2013-02-28 15:20:40 +0100

    Hra dáma

--- dama_pr2/src/dama_pr2/Dama_pr2.java
+++ dama_pr2/src/dama_pr2/Dama_pr2.java
@@ -5,6 +5,10 @@ package dama_pr2;
  *
  * @author Michal Herrman
  * Date: 20.2.2013
+ * 
+ * Dáma
+ * Protihráčem uživatele je počítač (reprezentovaný umělou inteligencí). 
+ * Hra se řídí podle určitých pravidel: http://cs.wikipedia.org/wiki/D%C3%A1ma_(hra).
  */
 public class Dama_pr2 {
 
